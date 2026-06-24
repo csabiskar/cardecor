09e1e29f61121edc437407fec1407ef1a1406ff1
csabiskar <csabiskar@gmail.com>
2026-06-24 09:39:52 +0530

    changes upadted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,21 @@
 import Image from "next/image";
 import logo from "@/assets/logo.png"; 
+
 export default function Footer() {
   return (
-    <footer className="bg-neutral-900 w-full  flex flex-col items-center justify-center gap-3">
+    <footer className="bg-neutral-900 w-full flex flex-col items-center justify-center ">
+      
+      <Image
+        src={logo}
+        alt="Car Decor"
+        width={100}
+        height={100}
+        className="object-contain"
+      />
 
-
-      <div className="relative w-52 h-20">
-        <Image
-          src={logo}
-          alt="Car Decor"
-          fill
-          className=" object-cover mt-8"
-        />
-      </div>
-
-    
-      <p className="text-neutral-400 text-sm text-center">
+      <p className="text-neutral-400 text-xs text-center">
         Copyright © 2025 Car Decor | All Rights Reserved
       </p>
-
     </footer>
   );
 }
